15f29703ddc8456db84f280ae8704d03c50ba7f8
Ajin Abraham <ajin25@gmail.com>
2018-01-28 01:25:16 +0530

    command line for DevSecOps

---
+++ cmd.py
@@ -0,0 +1,15 @@
+import json
+import argparse
+from core.scanner import general_code_analysis
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", nargs='+',
+                        help="Path to Node.js Source Code to Scan", required=True)
+    args = parser.parse_args()
+    if args.directory:
+    	print (args.directory)
+    	scan_results = general_code_analysis(args.directory)
+    	print (json.dumps(scan_results))
+    else:
+        parser.print_help()
