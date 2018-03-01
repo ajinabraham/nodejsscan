1f143c6084324d4d80684eecd03d88c92d7c1da3
Ajin Abraham <ajin25@gmail.com>
2018-03-01 10:58:27 +0530

    code QA

--- cli.py
+++ cli.py
@@ -6,15 +6,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", nargs='+',
                         help="Path to Node.js Source Code to Scan", required=True)
-    parser.add_argument("-r","--results",
-                        help="Generate Json report for DevSecOps CI/CD pipelines", required=False )
+    parser.add_argument("-r", "--results",
+                        help="Generate Json report for DevSecOps CI/CD pipelines", required=False)
     args = parser.parse_args()
-    if args.directory and not args.results:
+    if args.directory:
         scan_results = general_code_analysis(args.directory)
-        print (json.dumps(scan_results))
-    elif args.directory and args.results:
-    	scan_results = general_code_analysis(args.directory)
-    	with open(str(args.results)+".json", 'w') as outfile:
-			json.dump(scan_results, outfile,sort_keys=True, indent=4, separators=(',', ': '))
+        if args.results:
+            with open(str(args.results) + ".json", 'w') as outfile:
+                json.dump(scan_results, outfile, sort_keys=True,
+                          indent=4, separators=(',', ': '))
+        else:
+            print(json.dumps(scan_results, sort_keys=True,
+                             indent=4, separators=(',', ': ')))
     else:
         parser.print_help()
