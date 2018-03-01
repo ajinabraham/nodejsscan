cb9fb648712ac804814c6365fe02bdf2d652b5ba
Umar Farook <34654067+Umar-Farook-we45@users.noreply.github.com>
2018-03-01 10:53:08 +0530

    Json Report Generation Option is added for DevSecOps CI/CD pipeline (#48)

--- cli.py
+++ cli.py
@@ -6,9 +6,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", nargs='+',
                         help="Path to Node.js Source Code to Scan", required=True)
+    parser.add_argument("-r","--results",
+                        help="Generate Json report for DevSecOps CI/CD pipelines", required=False )
     args = parser.parse_args()
-    if args.directory:
+    if args.directory and not args.results:
         scan_results = general_code_analysis(args.directory)
         print (json.dumps(scan_results))
+    elif args.directory and args.results:
+    	scan_results = general_code_analysis(args.directory)
+    	with open(str(args.results)+".json", 'w') as outfile:
+			json.dump(scan_results, outfile,sort_keys=True, indent=4, separators=(',', ': '))
     else:
         parser.print_help()
