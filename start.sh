86eb50b7765140b84c23fc13f77bff5fe158e1a8
Ajin Abraham <ajin25@gmail.com>
2018-11-18 11:05:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ setsid docker-entrypoint.sh postgres >/dev/fd/1 2>&1 < /dev/fd/1 &
 
 sleep 10
 python3 migrate.py
-gunicorn -b 0.0.0.0:9090 app:app
+gunicorn -b 0.0.0.0:9090 app:app --workers 3 --timeout 10000
